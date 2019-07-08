ebc384d5c48ecca3fd89c87915421e97334f066a
mishalisovskiy <mishalisovskiy@gmail.com>
2019-07-08 13:16:29 +0300

    Backend ready

--- routes/data.js
+++ routes/data.js
@@ -1,11 +1,48 @@
 const router = require('express').Router();
+const fs = require('fs');
+const { promisify } = require('util');
+const path = require('path');
 
-const { arrayParser, Stack } = require('../utils/dataUtils');
+const { getLogEntries } = require('../utils/dataUtils');
+
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
 
 // route: POST /data
 // desc: posting new array of requests
 router.post('/', async (req, res) => {
-    const { logs } = req.body;
-    const parsedArray = arrayParser(logs);
-    // parsing here
+  const { entries } = req.body;
+  const dataRaw = await readFile(path.resolve(__dirname, '..', 'averagedLogs.json'));
+  const data = JSON.parse(dataRaw);
+  const logs = getLogEntries(entries);
+  
+  // Writing new entries to a file
+  logs.forEach((elem) => {
+    if (data[elem.url]) {
+      if (data[elem.url].logs && data[elem.url].logs.length >= 5) {
+        data[elem.url].logs.shift();
+        data[elem.url].logs.push({ "tsStart": elem.tsStart, "tsEnd": elem.tsEnd, "avgDelay": elem.avgDelay });
+      } else if (data[elem.url].logs) {
+        data[elem.url].logs.push({ "tsStart": elem.tsStart, "tsEnd": elem.tsEnd, "avgDelay": elem.avgDelay });
+      }
+    } else {
+      data[elem.url] = { "logs": [{ "tsStart": elem.tsStart, "tsEnd": elem.tsEnd, "avgDelay": elem.avgDelay }] };
+    }
+  });
+
+  // Calculating average delay for each URL
+  Object.entries(data).forEach(([url, info]) => {
+    const avgDelay = info.logs.reduce((acc, value) => { return acc + value.avgDelay }, 0) / info.logs.length;
+    data[url].avgDelay = avgDelay;
+  });
+
+  await writeFile(path.resolve(__dirname, '..', 'averagedLogs.json'), JSON.stringify(data));
+  res.json({ msg: 'Logs uploaded' });
 });
+
+router.get('/', async (req, res) => {
+  const data = await readFile(path.resolve(__dirname, '..', 'averagedLogs.json'));
+  res.json(JSON.parse(data));
+});
+
+module.exports = router;
--- utils/dataUtils.js
+++ utils/dataUtils.js
@@ -1,20 +1,19 @@
-export const arrayParser = array => array.reduce((acc, value) => {
-  const { url, timestamp, delay} = value;
-  if (acc[url]) {
-    return { ...acc, [url]: [ ...acc[url], { timestamp, delay } ] };
-  }
-
-  return { ...acc, [url]: [{ timestamp, delay }] };
-  }, {}
-);
-
-export const getLogEntries = object => {
-  return Object.entries(object).reduce((acc, value) => {
+const getLogEntries = array => {
+  const parsedArray = array.reduce((acc, value) => {
+    const { url, timestamp, delay} = value;
+    if (acc[url]) {
+      return { ...acc, [url]: [ ...acc[url], { timestamp, delay } ] };
+    }
+  
+    return { ...acc, [url]: [{ timestamp, delay }] };
+    }, {}
+  );
+  return Object.entries(parsedArray).reduce((acc, value) => {
     const [url, data] = value;
     let tsStart;
     let tsEnd;
     const delays = [];
-    const findMinAndMax = data.forEach(({timestamp, delay}, index) => {
+    data.forEach(({timestamp, delay}, index) => {
       if (index === 1) {
         tsStart = timestamp;
         tsEnd = timestamp;
@@ -34,14 +33,4 @@ export const getLogEntries = object => {
   }, []);
 };
 
-const arrayOfTestData = [ 
-  { url: 'first', timestamp: 562321946359, delay: 10 },
-  { url: 'first', timestamp: 562321946323, delay: 7 },
-  { url: 'first', timestamp: 562321946127, delay: 5 },
-  { url: 'second', timestamp: 562321946359, delay: 3 },
-  { url: 'second', timestamp: 562321946323, delay: 5 },
-  { url: 'second', timestamp: 562321946100, delay: 4 },
-  { url: 'third', timestamp: 562321941234, delay: 8 },
-  { url: 'third', timestamp: 562321943456, delay: 7 },
-  { url: 'third', timestamp: 562321942341, delay: 9 },
-];
+module.exports = { getLogEntries };
